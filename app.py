1c1e8c2dae3615222f8d73947467feee1053915f
iftikhar <iftikhar.islootech@gmail.com>
2025-12-06 19:39:18 +0500

    updated

--- app.py
+++ app.py
@@ -2,3 +2,10 @@ import streamlit as st
 
 st.title("Test App")
 st.write("If this works, then Streamlit is installed correctly!")
+
+# check if pandas is installed
+try:
+    import pandas as pd
+    st.write("Pandas is installed")
+except ImportError:
+    st.write("Pandas is not installed")
